f9837ad657524528fe3ece06006425f4a0023f9b
francomtn <fmontanelli@fi.uba.ar>
2024-05-24 20:05:58 -0300

    cola_prioridad

---
+++ tdas/cola_prioridad/cola_prioridad_test.go
@@ -0,0 +1,170 @@
+package cola_prioridad_test
+
+import (
+	TDAHeap "tdas/cola_prioridad"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	TEST_VOLUMEN int = 10000
+)
+
+func compararCadenas(cad1, cad2 string) int {
+	if cad1 < cad2 {
+		return -1
+	} else if cad1 > cad2 {
+		return 1
+	}
+	return 0
+}
+
+func compararEnteros(a, b int) int {
+	return a - b
+}
+
+func TestHeapVacio(t *testing.T) {
+
+	t.Log("Prueba que al crear un heap vacio, se comporte como corresponde")
+	heap := TDAHeap.CrearHeap(compararEnteros)
+	require.True(t, heap.EstaVacia())
+	require.PanicsWithValue(t, "La cola esta vacia", func() { heap.VerMax() })
+	require.PanicsWithValue(t, "La cola esta vacia", func() { heap.Desencolar() })
+	require.Equal(t, 0, heap.Cantidad())
+}
+
+func TestEncolar(t *testing.T) {
+
+	heap := TDAHeap.CrearHeap(compararEnteros)
+
+	arreglo := []int{3, 5, 0, 8, 11, 23, 54, 17}
+	for _, i := range arreglo {
+		heap.Encolar(i)
+	}
+	require.False(t, heap.EstaVacia())
+	require.Equal(t, 8, heap.Cantidad())
+	require.Equal(t, 54, heap.VerMax())
+}
+
+func TestDesencolar(t *testing.T) {
+
+	arregloOrdenado := []int{54, 23, 17, 11, 8, 5, 3, 0}
+
+	heap := TDAHeap.CrearHeap(compararEnteros)
+	test := []int{3, 5, 0, 8, 11, 23, 54, 17}
+
+	for _, i := range test {
+		heap.Encolar(i)
+	}
+
+	require.Equal(t, 8, heap.Cantidad())
+
+	for _, elem := range arregloOrdenado {
+		require.Equal(t, elem, heap.Desencolar())
+	}
+}
+
+func TestHeapDeCadenas(t *testing.T) {
+
+	t.Log("Pruebas primitivas de heap de strings")
+	heap := TDAHeap.CrearHeap(compararCadenas)
+	require.True(t, heap.EstaVacia())
+	require.PanicsWithValue(t, "La cola esta vacia", func() { heap.VerMax() })
+	require.PanicsWithValue(t, "La cola esta vacia", func() { heap.Desencolar() })
+	require.EqualValues(t, 0, heap.Cantidad())
+
+	heap.Encolar("Algoritmos")
+	require.False(t, heap.EstaVacia())
+	require.Equal(t, 1, heap.Cantidad())
+	require.Equal(t, "Algoritmos", heap.VerMax())
+
+	heap.Encolar("Estructuras")
+	require.Equal(t, 2, heap.Cantidad())
+	require.Equal(t, "Estructuras", heap.VerMax())
+
+	heap.Encolar("Hola")
+	require.Equal(t, 3, heap.Cantidad())
+	require.Equal(t, "Hola", heap.VerMax())
+
+	heap.Encolar("Mundo")
+	require.Equal(t, 4, heap.Cantidad())
+	require.Equal(t, "Mundo", heap.VerMax())
+
+	heap.Encolar("Datos")
+	require.Equal(t, 5, heap.Cantidad())
+	require.Equal(t, "Mundo", heap.VerMax())
+
+	require.Equal(t, "Mundo", heap.Desencolar())
+	require.Equal(t, 4, heap.Cantidad())
+	require.Equal(t, "Hola", heap.VerMax())
+	require.Equal(t, "Hola", heap.Desencolar())
+	require.Equal(t, 3, heap.Cantidad())
+	require.Equal(t, "Estructuras", heap.VerMax())
+	require.Equal(t, "Estructuras", heap.Desencolar())
+	require.Equal(t, 2, heap.Cantidad())
+	require.Equal(t, "Datos", heap.VerMax())
+	require.Equal(t, "Datos", heap.Desencolar())
+	require.Equal(t, 1, heap.Cantidad())
+	require.Equal(t, "Algoritmos", heap.VerMax())
+	require.Equal(t, "Algoritmos", heap.Desencolar())
+	require.Equal(t, 0, heap.Cantidad())
+	require.True(t, heap.EstaVacia())
+}
+
+func TestVolumen(t *testing.T) {
+
+	t.Log("Pruebas de volumen de heap")
+
+	heap := TDAHeap.CrearHeap(compararEnteros)
+
+	for i := 0; i < TEST_VOLUMEN; i++ {
+		heap.Encolar(i)
+		require.Equal(t, i, heap.VerMax())
+		require.Equal(t, i+1, heap.Cantidad())
+	}
+
+	for i := TEST_VOLUMEN; i > 0; i-- {
+		require.Equal(t, i-1, heap.VerMax())
+		require.Equal(t, i-1, heap.Desencolar())
+		require.Equal(t, i-1, heap.Cantidad())
+	}
+	require.True(t, heap.EstaVacia())
+}
+
+func TestCrearHeapArregloVacio(t *testing.T) {
+	t.Log("Prueba crea un heap con un arreglo vacio")
+	arr := []int{}
+	heap := TDAHeap.CrearHeapArr(arr, compararEnteros)
+	require.True(t, heap.EstaVacia())
+	require.PanicsWithValue(t, "La cola esta vacia", func() { heap.VerMax() })
+	require.PanicsWithValue(t, "La cola esta vacia", func() { heap.Desencolar() })
+	require.EqualValues(t, 0, heap.Cantidad())
+
+	heap.Encolar(9)
+	require.False(t, heap.EstaVacia())
+	require.EqualValues(t, 9, heap.VerMax())
+	require.EqualValues(t, 1, heap.Cantidad())
+}
+
+func TestHeapsort(t *testing.T) {
+	test := []int{3, 5, 0, 8, 11, 23, 54, 17}
+	arregloOrdenado := []int{0, 3, 5, 8, 11, 17, 23, 54}
+
+	TDAHeap.HeapSort(test, compararEnteros)
+	require.Equal(t, arregloOrdenado, test)
+}
+
+func TestCrearHeapArreglo(t *testing.T) {
+	t.Log("Prueba crea un heap con un arreglo de enteros")
+
+	arregloOrdenado := []int{54, 23, 17, 11, 8, 5, 3, 0}
+	test := []int{3, 5, 0, 8, 11, 23, 54, 17}
+
+	heap := TDAHeap.CrearHeapArr(test, compararEnteros)
+
+	for _, elem := range arregloOrdenado {
+		require.Equal(t, elem, heap.Desencolar())
+	}
+	require.True(t, heap.EstaVacia())
+}
---
+++ tdas/cola_prioridad/heap.go
@@ -0,0 +1,158 @@
+package cola_prioridad
+
+const (
+	CAPACIDAD_INICIAL int = 10
+	CANT_INICIAL      int = 0
+	COEF_REDIMENSION  int = 4
+	VALOR_REDIMENSION int = 2
+)
+
+type colaConPrioridad[T any] struct {
+	datos []T
+	cant  int
+	cmp   func(T, T) int
+}
+
+func CrearHeap[T any](funcion_cmp func(T, T) int) ColaPrioridad[T] {
+	nuevo := make([]T, CAPACIDAD_INICIAL)
+	return &colaConPrioridad[T]{datos: nuevo, cant: CANT_INICIAL, cmp: funcion_cmp}
+}
+
+func CrearHeapArr[T any](arreglo []T, funcion_cmp func(T, T) int) ColaPrioridad[T] {
+	var datos []T
+	if len(arreglo) == 0 {
+		datos = make([]T, CAPACIDAD_INICIAL)
+	} else {
+		datos = make([]T, len(arreglo))
+		copy(datos, arreglo)
+		heapify(datos, funcion_cmp)
+	}
+	return &colaConPrioridad[T]{datos: datos, cant: len(arreglo), cmp: funcion_cmp}
+}
+
+func (heap *colaConPrioridad[T]) Cantidad() int {
+	return heap.cant
+}
+
+func (heap *colaConPrioridad[T]) EstaVacia() bool {
+	return heap.cant == CANT_INICIAL
+}
+
+func (heap *colaConPrioridad[T]) VerMax() T {
+
+	if heap.EstaVacia() {
+		panic("La cola esta vacia")
+	}
+
+	return heap.datos[0]
+}
+
+func (heap *colaConPrioridad[T]) Encolar(dato T) {
+
+	cap := len(heap.datos)
+	if heap.cant == cap {
+		heap.redimensionar(heap.cant * VALOR_REDIMENSION)
+	}
+
+	heap.datos[heap.cant] = dato
+	heap.cant++
+	upHeap(heap.cant-1, heap.datos, heap.cmp)
+
+}
+
+func (heap *colaConPrioridad[T]) Desencolar() T {
+
+	if heap.EstaVacia() {
+		panic("La cola esta vacia")
+	}
+
+	dato := heap.datos[0]
+	heap.cant--
+	if heap.cant > 0 {
+		heap.datos[0] = heap.datos[heap.cant]
+		downHeap(0, heap.datos[:heap.cant], heap.cmp)
+	}
+
+	cap := len(heap.datos)
+	if heap.cant*COEF_REDIMENSION <= cap && cap > CAPACIDAD_INICIAL {
+		heap.redimensionar(cap / VALOR_REDIMENSION)
+	}
+
+	return dato
+}
+
+func HeapSort[T any](elementos []T, funcion_cmp func(T, T) int) {
+	if len(elementos) < 2 {
+		return
+	}
+	heapify(elementos, funcion_cmp)
+	for i := len(elementos) - 1; i > 0; i-- {
+		swap(&elementos[0], &elementos[i])
+		downHeap(0, elementos[:i], funcion_cmp)
+	}
+}
+
+func swap[T any](dato1 *T, dato2 *T) {
+	*dato1, *dato2 = *dato2, *dato1
+}
+
+func downHeap[T any](i int, arr []T, func_cmp func(T, T) int) {
+
+	if i >= len(arr) || i < 0 {
+		return
+	}
+
+	hijoIzq := 2*i + 1
+	hijoDer := 2*i + 2
+	mayor := i
+
+	if hijoIzq < len(arr) && func_cmp(arr[hijoIzq], arr[mayor]) > 0 {
+		mayor = hijoIzq
+	}
+
+	if hijoDer < len(arr) && func_cmp(arr[hijoDer], arr[mayor]) > 0 {
+		mayor = hijoDer
+	}
+
+	if mayor != i {
+		swap(&arr[i], &arr[mayor])
+		downHeap(mayor, arr, func_cmp)
+	}
+}
+
+func upHeap[T any](i int, arr []T, func_cmp func(T, T) int) {
+
+	padre, iPadre, tienePadre := obtenerPadre(i, arr)
+
+	if !tienePadre || func_cmp(*padre, arr[i]) > 0 {
+		return
+	}
+
+	swap(&arr[i], &arr[iPadre])
+	upHeap(iPadre, arr, func_cmp)
+}
+
+func heapify[T any](arr []T, func_cmp func(T, T) int) {
+
+	for i := len(arr) - 1; i >= 0; i-- {
+		downHeap(i, arr, func_cmp)
+	}
+}
+
+func obtenerPadre[T any](i int, arr []T) (*T, int, bool) {
+
+	iPadre := (i - 1) / 2
+
+	if i == 0 {
+		return &arr[0], 0, false
+	}
+
+	return &arr[iPadre], iPadre, true
+}
+
+func (heap *colaConPrioridad[T]) redimensionar(nuevaCapacidad int) {
+
+	nuevo := make([]T, nuevaCapacidad)
+	copy(nuevo, heap.datos)
+	heap.datos = nuevo
+}
